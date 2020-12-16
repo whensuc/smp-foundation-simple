9bf24df7a0f91a77cd8b56db7c8a86586f18d6e5
wzy <wangzhiyuan@hec.cn>
2020-12-16 08:05:58 +0800

    增加工具类

---
+++ src/main/java/cn/com/dyg/work/utils/PublicUtil.java
@@ -0,0 +1,91 @@
+package cn.com.dyg.work.utils;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
+
+
+import org.springframework.util.StringUtils;
+
+
+public class PublicUtil {
+
+    /**
+     * 获取用户真实IP地址，不使用request.getRemoteAddr();的原因是有可能用户使用了代理软件方式避免真实IP地址,
+     *
+     * 可是，如果通过了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP值，究竟哪个才是真正的用户端的真实IP呢？
+     * 答案是取X-Forwarded-For中第一个非unknown的有效IP字符串。
+     *
+     * 如：X-Forwarded-For：192.168.1.110, 192.168.1.120, 192.168.1.130,
+     * 192.168.1.100
+     *
+     * 用户真实IP为： 192.168.1.110
+     *
+     * @param request
+     * @return
+     */
+//    public static String getIpAddress(HttpServletRequest request) {
+//        String ip = request.getHeader("x-forwarded-for");
+//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//            ip = request.getHeader("Proxy-Client-IP");
+//        }
+//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//            ip = request.getHeader("WL-Proxy-Client-IP");
+//        }
+//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//            ip = request.getHeader("HTTP_CLIENT_IP");
+//        }
+//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+//        }
+//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//            ip = request.getRemoteAddr();
+//        }
+//        return ip;
+//    }
+
+
+    public static InputStream Base64ToInputStream(String base64img) { // 对字节数组字符串进行Base64解码并生成图片
+        InputStream in = null;
+        if (StringUtils.isEmpty(base64img)) // 图像数据为空
+            return null;
+
+        try {
+            // Base64解码
+            byte[] b = org.apache.commons.codec.binary.Base64.decodeBase64(base64img);
+//            for (int i = 0; i < b.length; ++i) {
+//                if (b[i] < 0) {// 调整异常数据
+//                    b[i] += 256;
+//                }
+//            }
+            in = new ByteArrayInputStream(b);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return in;
+    }
+
+
+    public static byte[] read(InputStream inputStream) throws IOException {
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int num = inputStream.read(buffer);
+            while (num != -1) {
+                baos.write(buffer, 0, num);
+                num = inputStream.read(buffer);
+            }
+            baos.flush();
+            return baos.toByteArray();
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+    }
+
+
+}
